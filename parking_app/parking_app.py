1dfb1387945396643d85e481f1464789b497e6c1
LAGxHATER <nwarren251@gmail.com>
2022-01-03 21:35:58 -0800

    fixed address bug

--- parking_app/parking_app.py
+++ parking_app/parking_app.py
@@ -23,24 +23,28 @@ soup = BeautifulSoup(page_html, "html.parser")
 #grabs all parking 
 parking = soup.findAll("div", {"class":"MVVflb-haAclf V0h1Ob-haAclf-d6wfac MVVflb-haAclf-uxVfW-hSRGPd"})
 
-#this variable is a counter
+#this is a count for loop
 counter = 0
-
 for container in parking:
+
+    #stop loop after 3 iterations
+    if counter == 3:
+        break
+
     #retrieves title
     name_container = container.findAll("span", {"jstcache":"77"})
     name = name_container[0].text
 
     #retrieves address
     address_container = container.findAll("span", {"jstcache":"104"})
-    address = address_container[1].text
+    #the address is stored as the second value in the array, meaning if the array len is == 1, no address is available
+    if len(address_container) == 1:
+        address = "*****ERROR: Address not listed*****"
+    elif len(address_container) > 1:
+        address = address_container[1].text
 
+    #update counter
     counter += 1
-
-    #stop loop after 3 iterations
-    if counter == 4:
-        break
-
     
     #printing 
     print("\n")
