9a3fae084ba40e4c208a0ae083a31b6f1010c306
LAGxHATER <nwarren251@gmail.com>
2022-01-05 08:16:27 -0800

    comments

--- parking_app/parking_app.py
+++ parking_app/parking_app.py
@@ -1,4 +1,4 @@
-#app that takes user location and prints out 3 nearby parking locations
+# app that takes user location and prints out 3 nearby parking locations
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
@@ -6,14 +6,14 @@ import re
 import urllib.parse
 
 switch = True
-#let the user enter their location via zipcode
+# let the user enter their location via zipcode
 while switch:
     user_input = input("Enter your zip code: ")
 
-    #encode user input
+    # encode user input
     user_input = urllib.parse.quote(user_input)
 
-    #validating zip code with regex
+    # validating zip code with regex
     match = re.search('^\d{5}(-\d{4})?$', user_input)
 
     if match:
@@ -22,54 +22,54 @@ while switch:
         print("ERROR: Zip Code Invalid")
         switch = True
 
-#this will be added onto the search link
+# this will be added onto the search link
 query = "Parking+" + user_input
 
 url = 'https://www.google.com/maps/search/' + query
 
-#headless makes browser invisible
+# headless makes browser invisible
 options = Options()
 options.headless = True
-#opening connection, grabbing the page with selenium
+# opening connection, grabbing the page with selenium
 driver = webdriver.Firefox(options=options)
 driver.get(url)
 
-#html parsing using beautifulsoup
+# html parsing using beautifulsoup
 page_html = driver.page_source
 soup = BeautifulSoup(page_html, "html.parser")
 
-#close all browser windows
+# close all browser windows
 driver.quit()
 
-#grabs all parking 
+# grabs all parking
 parking = soup.findAll("div", {"class":"MVVflb-haAclf V0h1Ob-haAclf-d6wfac MVVflb-haAclf-uxVfW-hSRGPd"})
 
-#this is a count for loop
+# this is a count for loop
 counter = 0
 for container in parking:
 
-    #stop loop after 3 iterations
+    # stop loop after 3 iterations
     if counter == 3:
         break
 
-    #retrieves title
-    name_container = container.findAll("span", {"jstcache":"77"})
+    # retrieves title
+    name_container = container.findAll("span", {"jstcache": "77"})
     name = name_container[0].text
 
-    #retrieves address
-    parking = container.findAll("span", {"jsinstance":"*1"})
-    address_container = container.findAll("span", {"jstcache":"104"})
+    # retrieves address
+    parking = container.findAll("span", {"jsinstance": "*1"})
+    address_container = container.findAll("span", {"jstcache": "104"})
 
-    #the address is stored as the second value in the array, meaning if the array len is == 1, no address is available
+    # the address is stored as the second value in the array, meaning if the array len is == 1, no address is available
     if len(address_container) == 1:
         address = "*****ERROR: Address not available*****"
     elif len(address_container) > 1:
         address = address_container[1].text
 
-    #update counter
+    # update counter
     counter += 1
 
-    #printing 
+    # printing
     print("\n")
     print("Place: " + name)
     print("Address: " + address)
